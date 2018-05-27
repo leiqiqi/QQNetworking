94969db7ad216103fc9ad38ca1345e34803aa120
1079035762@qq.com <1079035762@qq.com>
2018-05-27 15:51:59 +0800

    添加代码注释

--- QQNetworking/QQNetworking/ViewController.m
+++ QQNetworking/QQNetworking/ViewController.m
@@ -30,28 +30,28 @@
     
 //    [self.modelService getRequestPieData:^(BOOL success, id result, NSString *message) {
 //        if (success) {
-////            NSLog(@"result+++++++++%@", result);
+// //            NSLog(@"result+++++++++%@", result);
 //            self.seriseArray = [result objectForKey:@"series"];
 //            [self getData];
 //        }
 //        else{
-//
+
 //        }
 //    }];
     
     
     
     
-//   图片上传
-//    UIImage *image = [UIImage imageNamed:@"动态 "];
+// //   图片上传
+//    UIImage *image = [UIImage imageNamed:@"动态"];
 //    NSArray *array = [NSArray arrayWithObjects:image, nil];
 //    [self.modelService uploadImageWithImageArray:array finished:^(BOOL success, id result, NSString *message) {
-//        
+       
 //        if (success) {
-//            
+           
 //        }
 //        else{
-//            
+           
 //        }
 //    }];
     
